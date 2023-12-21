ce36e78cac9f539a418cdc95a2aed9e419dd9ff3
HidriSana <115165735+HidriSana@users.noreply.github.com>
2023-12-21 10:09:45 +0100

    Added a fake productFixture with category related

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -4,11 +4,14 @@ namespace App\DataFixtures;
 
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 ;
 
-class CategoryFixtures extends Fixture
+class CategoryFixtures extends Fixture 
 {
+    public const CATEGORY_HIGHTECH= "CATEGORY_HIGHTECH";
+
     public function load(ObjectManager $manager): void
     {
         $toys = new Category();
@@ -22,7 +25,8 @@ class CategoryFixtures extends Fixture
         $hightech = new Category();
         $hightech -> setName('Hightech');
         $manager ->persist($hightech);
-        
+        $this->addReference(self::CATEGORY_HIGHTECH, $hightech);
+
         $gaming = new Category();
         $gaming-> setName('Gaming');
         $gaming-> setParent($hightech);
--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -4,20 +4,25 @@ namespace App\DataFixtures;
 
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Migrations\Exception\DependencyException;
 use Doctrine\Persistence\ObjectManager;
 
 
-class ProductFixtures extends Fixture
+class ProductFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        /*$iphone13 = new Product();
+        $iphone13 = new Product();
         $iphone13-> setName('Iphone13');
         $iphone13-> setPrice('500');
         $iphone13-> setDescription("Description de l'iphone 13");
-        //La relation ici 
+        $iphone13->setCategory($this->getReference(CategoryFixtures::CATEGORY_HIGHTECH));
         $manager-> persist($iphone13);
 
-        $manager->flush();*/
+        $manager->flush();
+    }
+    public function getDependencies(): array {
+        return [CategoryFixtures::class];
     }
 }
